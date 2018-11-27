a1f58a8df525216edfbed1ebd0caf8c6b135e3b0
bowenbowensun <bs2bg@virginia.edu>
2018-11-27 13:26:13 -0500

    dns server and project 1 in python3

--- 1-web-server.py
+++ 1-web-server.py
@@ -12,49 +12,55 @@ import errno
 import signal
 import socket
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 54321              # Arbitrary non-privileged port (port number > 1024)
+HOST = '127.0.0.1'  # Symbolic name meaning all available interfaces
+PORT = 3333  # Arbitrary non-privileged port (port number > 1024)
 
 HTML_BODY = 'Secret black Site<br>'
-def HTML_RESPONSE(ip_address):
-	HTML_RESPONSE = 'HTTP/1.0 200 OK\r\n'
-	HTML_RESPONSE += 'Content-Type:text/html\r\n'
-	HTML_RESPONSE += 'Connection:close\r\n\n'
-	HTML_RESPONSE += '<html><head>' 
-	HTML_RESPONSE += HTML_BODY
-	HTML_RESPONSE += ip_address
-	HTML_RESPONSE += '</head></html>\r\n'
-	return HTML_RESPONSE
+
+
+def html_response(ip_address):
+    html_message = 'HTTP/1.0 200 OK\r\n'
+    html_message += 'Content-Type:text/html\r\n'
+    html_message += 'Connection:close\r\n\n'
+    html_message += '<html><head>'
+    html_message += HTML_BODY
+    html_message += ip_address
+    html_message += '</head></html>\r\n'
+    return html_message
+
 
 def create_server_socket():
-	s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-	s.bind((HOST, PORT))
-	s.listen(1)
-	return s
+    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    return s
+
 
 def wait_for_client(s):
-	while True:
-		try:
-		  conn_sock, (src_ip, src_port) = s.accept()
-		  conn_sock.sendall(HTML_RESPONSE(src_ip))
-		  conn_sock.shutdown(socket.SHUT_WR)
-		  # Why doesn't this work when I call conn_sock.close()?
-		  # And why does each browser request happen twice?
-		  print('Responded to', src_ip + ':' + str(src_port))
-		except socket.error as (code, msg):
-			exit(-1)
+    while True:
+        try:
+            conn_sock, (src_ip, src_port) = s.accept()
+            conn_sock.sendall(html_response(src_ip).encode())
+            conn_sock.shutdown(socket.SHUT_WR)
+            # Why doesn't this work when I call conn_sock.close()?
+            # And why does each browser request happen twice?
+            print('Responded to', src_ip + ':' + str(src_port))
+        except socket.error:
+            exit(-1)
 
 
 def handle_sigint(s):
-	def handler(signum, frame):
-		s.close()
-		print '\nSocket closed.'
-	signal.signal(signal.SIGINT, handler)
-	print 'registered handler'
+    def handler(signum, frame):
+        s.close()
+        print('\nSocket closed.')
+    signal.signal(signal.SIGINT, handler)
+    print('registered handler')
+
 
 def main():
-	socket = create_server_socket()
-	handle_sigint(socket)
-	wait_for_client(socket)
+    server_socket = create_server_socket()
+    handle_sigint(server_socket)
+    wait_for_client(server_socket)
+
 
 main()
