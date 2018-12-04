0b417d68583eda5ecef07b6ab6896a065c7cf941
Jack Morris <jxmorris12@gmail.com>
2018-12-04 15:47:31 -0500

    otp generate_from_key

--- one_time_pad.py
+++ one_time_pad.py
@@ -6,6 +6,8 @@ ascii_end = 122
 
 ascii_space = 32
 
+ascii_jump = 17
+
 def random_ascii_int():
 	i = randint(ascii_beg - 1, ascii_end)
 	# space is not in our contiguous range of allowed ascii characters 
@@ -19,6 +21,15 @@ def random_ascii():
 # generate a one time pad ASCII string
 def generate(length):
 	return ''.join([random_ascii() for _ in range(length)])
+
+# generate a new one time pad from an old ascii string
+def generate_from_key(key):
+	s = ''
+	for c in key:
+		i = ord(c)+ascii_jump
+		if i >= ascii_end: i -= (ascii_end - ascii_beg)
+		s += chr(i)
+	return s
 
 # generate and save a one time pad ASCII string
 def generate_and_save(length):
@@ -38,3 +49,7 @@ def strong_encrypt(string):
 # encrypt or decrypt a string with one time pad
 def crypto(string, secret_key):
 	return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(string, secret_key))
+
+# print an xor key as ascii
+def ascii_key(key):
+	return ''.join(chr(ord(c) + ascii_beg) for c in key)
